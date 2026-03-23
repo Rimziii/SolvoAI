cc10ac567e03aa2e7128a91f7ebfe51e24142d00
Akanksha Dubey <rimzi2603@gmail.com>
2026-03-23 13:16:01 +0530

    fix landing page route

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.use(bodyParser.json({ limit: '200kb' }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
